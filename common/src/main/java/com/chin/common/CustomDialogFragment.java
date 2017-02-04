19ce8683805956be810a06ad1bc73a9d3025ac4a
Trung Do <chin.bimbo@gmail.com>
2017-02-04 01:35:08 -0500

    Fix lint: use newInstance() and default ctor for CustomDialogFragment

--- common/src/main/java/com/chin/common/CustomDialogFragment.java
+++ common/src/main/java/com/chin/common/CustomDialogFragment.java
@@ -1,24 +1,30 @@
 package com.chin.common;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 
-@SuppressLint("NewApi")
+@RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
 public class CustomDialogFragment extends DialogFragment {
-    String message;
+    private static String MESSAGE = "message";
 
-    public CustomDialogFragment(String message) {
-        this.message = message;
+    public static CustomDialogFragment newInstance(String message) {
+        CustomDialogFragment fragment = new CustomDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(MESSAGE, message);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        String message = getArguments().getString(MESSAGE);
         builder.setMessage(message)
                .setPositiveButton("Quit", new DialogInterface.OnClickListener() {
                    @Override
